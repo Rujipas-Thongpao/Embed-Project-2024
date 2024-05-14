956df1da73360e377166218e17e7a0ecf5c49ace
paopeaw <paopeaw8246@gmail.com>
2024-05-14 13:56:39 +0700

    idk

---
+++ embed-website/src/components/predictLogo.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="../assets/weather.webp" />
+</template>
+
+
+<script>
+  import forecastData from '../assets/forecast.json'
+  export default {
+    props: {
+      z: {
+        type: Number,
+        required: true
+      },
+    },
+
+  }
+</script>
+
+
+
